a4febf837a6bf2cf0a3cf9d8380e12cd29271c9b
SavvasAjaiRamalingam <ajai.ramalingam@savvas.com>
2021-06-07 17:56:10 +0530

    small change on delete api

--- server/controllers/message.js
+++ server/controllers/message.js
@@ -34,37 +34,6 @@ router.post('/getMessage', async (req, res) => {
     })
 });
 
-router.post('/deleteMessage', async (req, res) => {
-    const { sender, reciver } = req.body;
-    jwt.verify(req.headers.token, 'doodleCloudKey', async (err, authData) => {
-        if (err) {
-            res.send({ success: false, message: 'Session out please login' });
-        } else {
-            await Message.find({ room: { $in: [`${sender}-${reciver}`, `${reciver}-${sender}`] }, active: true },
-                (err, response) => {
-                    if (response) {
-                        return res.send(
-                            {
-                                message: 'Successfully get the message.',
-                                success: true,
-                                data: response
-                            }
-                        );
-                    } else {
-                        return res.send(
-                            {
-                                message: 'No message found',
-                                success: false
-                            }
-                        );
-                    }
-                }
-
-            );
-        }
-    })
-});
-
 router.post('/deleteMsg', async (req, res) => {
     const { seletedIds } = req.body;
     jwt.verify(req.headers.token, 'doodleCloudKey', async (err, authData) => {
